85984d5a022b3ce587caaddf848e354e1fb84616
Christopher Luna <chris@illusionlayervfx.com>
2019-03-07 04:56:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def square_array(array)
 #array.each do |square|
-   array.each do |square|
-     array.sort(square*square)
+   array.each {|square| square*square }
+     array.fill
 end
 end
